62c354ee733573456273fd4afd6f085830911b02
lyczos <lyczos@gmail.com>
2020-05-17 22:03:35 +0200

    update dosc

--- sentry-release.sh
+++ sentry-release.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # Send release information to sentry
-# ./sentry-release.sh production COMMIT_HASH
+# ./sentry-release.sh production
 
-# Sometimes you want to you Sentry from NPM
-# but sometimes maybe you have it already so you can change it here to
-# SENTRY_PATH="/usr/local/bin/sentry-cli";
-# Depends on you case.
-SENTRY_PATH="./node_modules/.bin/sentry-cli"
-GIT_REPO="lyczos/liki-devtalk-sentry" # we need it if we want to associate releases with commits/versions
-DIST_DIR="dist/liki-devtalk-sentry/"
-FILENAME_WITH_VERSION="RELEASE_VERSION" # name of file that's includes app version number/hash
+# If you use Sentry-cli from the NPM package added to the project,
+# leave the current value. However, sometimes you can have it installed earlier
+# in this case set the path here or just enter "sentry-cli"
+SENTRY="./node_modules/.bin/sentry-cli"
+
+# if we want to associate releases with commits/versions
+GIT_REPO="lyczos/liki-devtalk-sentry"
+
+# if is because our app is under sub directory https://lyczos.github.io/liki-devtalk-sentry/
+# if we have our app in main directory (ex https://lyczos.github.io/ )
+# we should update it to "dist/liki-devtalk-sentry/":
+DIST_DIR="dist/"
+
+# Name of file that's includes app version number/hash
+# You can generate that file during build process and set version to ex. 1.0.2
+# in case when file doesn't exist, script will try to use most recent commit hash
+# in place of version
+FILENAME_WITH_VERSION="RELEASE_VERSION"
 
 ENV_VAR=$1
 if [ -n "$ENV_VAR" ]; then
@@ -19,18 +29,18 @@ if [ -n "$ENV_VAR" ]; then
     echo "Version was taken from $FILENAME_WITH_VERSION file:  $VERSION"
   else
     # Take app version form sentry-cli (it will use most recent commit hash)
-    VERSION=$($SENTRY_PATH releases propose-version)
+    VERSION=$($SENTRY releases propose-version)
     echo "Version was taken from GIT: $VERSION"
   fi
   # Start new release (for Sentry)
-  ./node_modules/.bin/sentry-cli releases new "$VERSION"
+  $SENTRY releases new "$VERSION"
   # Upload source maps
-  ./node_modules/.bin/sentry-cli releases files "$VERSION" upload-sourcemaps --rewrite $DIST_DIR # Path to bundle
+  $SENTRY releases files "$VERSION" upload-sourcemaps --rewrite $DIST_DIR # Path to bundle
   # Set up version to enhance error targeting
-  ./node_modules/.bin/sentry-cli releases set-commits "$VERSION" --commit "$GIT_REPO""@""$VERSION"
+  $SENTRY releases set-commits "$VERSION" --commit "$GIT_REPO""@""$VERSION"
   # Finish release and send all info to sentry.io
-  ./node_modules/.bin/sentry-cli releases finalize "$VERSION"
-  ./node_modules/.bin/sentry-cli releases deploys "$VERSION" new -e "$ENV_VAR"
+  $SENTRY releases finalize "$VERSION"
+  $SENTRY releases deploys "$VERSION" new -e "$ENV_VAR"
 else
     echo "ERROR: Provide an environment. Ex. './sentry-release.sh production'"
 fi
